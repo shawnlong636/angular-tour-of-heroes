14e201d29bd4ec3629e94cd91aaa94609ade6974
Shawn Long <shawnlong@Shawns-MacBook-Pro.local>
2022-11-21 14:23:22 -0800

    Set up In Memory Data Service to Mimic requesting data from server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs'
@@ -8,19 +10,26 @@ import { MessageService } from './message.service'
   providedIn: 'root'
 })
 export class HeroService {
+  private readonly heroesUrl = 'api/heroes' // URL to web
+
   constructor (
-    private readonly messageService: MessageService
+    private readonly messageService: MessageService,
+    private readonly http: HttpClient
   ) { }
 
   getHeroes (): Observable<Hero[]> {
-    const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes')
+    const heroes = this.http.get<Hero[]>(this.heroesUrl)
+    this.log('fetched Heroes')
     return heroes
   }
 
   getHero (id: number): Observable<Hero> {
     const hero = HEROES.find((hero): boolean => { return hero.id === id })!
-    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    this.log(`fetched hero id=${id}`)
     return of(hero)
+  }
+
+  private log (message: string): void {
+    this.messageService.add(`HeroService: ${message}`)
   }
 }
--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing'
 
-import { MessageService } from './message.service'
+import { InMemoryDataService } from './in-memory-data.service'
 
-describe('MessageService', () => {
-  let service: MessageService
+describe('InMemoryDataService', () => {
+  let service: InMemoryDataService
 
   beforeEach(() => {
     TestBed.configureTestingModule({})
-    service = TestBed.inject(MessageService)
+    service = TestBed.inject(InMemoryDataService)
   })
 
   it('should be created', () => {
